40504f848413b08a62c3a9b07dc15b0a4216bf18
Atharva Vaidya <atharva.vaidya@icloud.com>
2020-01-13 22:30:23 +0530

    Initial Commit

---
+++ Gnosis-Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gnosis-Task
+//
+//  Created by Atharva Vaidya on 1/13/20.
+//  Copyright © 2020 Atharva Vaidya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
